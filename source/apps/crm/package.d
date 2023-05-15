ecbc1d61dab061d4224bb3c79ea51a8d642a2ee4
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-15 03:41:04 +0200

    Update package.d

--- source/apps/crm/package.d
+++ source/apps/crm/package.d
@@ -1,7 +1,33 @@
 module apps.crm;
 
-@safe:
-public import apps.crm.classes; 
-public import apps.crm.controllers; 
-public import apps.crm.helpers; 
-public import apps.crm.views; 
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.crm.controllers;
+  import apps.crm.helpers;
+  import apps.crm.tests;
+  import apps.crm.views;
+}
+
+DApplication crmApp;
+static this() {
+  portalsApp = Application
+    .name("crmApp")
+    .rootPath("/apps/crm")
+    .addRoute(Route("/", HTTPMethod.GET, CRMIndexPageController));
+}
